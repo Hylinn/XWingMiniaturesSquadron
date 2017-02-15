e49ab6180d71a2e60064477e68084694a3eb0ec7
Hylinn Taggart <htaggart@box.com>
2017-02-15 10:42:04 -0800

    Added UpgradeSlot

---
+++ Java/src/main/java/io/github/hylinn/xwing/upgrade/UpgradeSlot.java
@@ -0,0 +1,24 @@
+package io.github.hylinn.xwing.upgrade;
+
+public class UpgradeSlot {
+
+    public enum Type {
+        ELITE,
+        ASTROMECH,
+        TORPEDO,
+        MISSILE,
+        CANNON,
+        TURRET,
+        BOMB,
+        CREW,
+        SALVAGED_ASTROMECH,
+        SYSTEM,
+        TITLE,
+        MODIFICATION,
+        ILLICIT,
+        CARGO,
+        HARDPOINT,
+        TEAM,
+        TECH
+    }
+}
